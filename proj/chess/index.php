3b524f013e250a9cd96610b053b8bac7668eba09
oliegjio <oliegjio@gmail.com>
2016-05-10 22:28:19 +0300

    a

--- proj/chess/index.php
+++ proj/chess/index.php
@@ -5,14 +5,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="shortcut icon" type="image/png" href="../../images/favicon.png">
-    <title>HAH</title>
+    <link rel="shortcut icon" type="image/png" href="../../../../images/favicon.png">
+    <title>Chess</title>
   </head>
   <body>
     <a href="../../index.html" style="position: fixed; right: 0px; background: orange; font-size: 30px; font-family: georgia; text-align: center; width: 42px; height: 42px; display: block; top: 0px">H</a>
-    <ul class="menu">
-      <li class="indent"><a href="proj/chess_ajax/index.php" class="chess">Шахмать</a>&nbsp&nbsp&nbsp<span class="chess_ajax">(AJAX)</span></li>
-      <li class="indent"><a href="proj/chess_websocket/index.php" class="chess">Шахматъ</a>&nbsp&nbsp&nbsp<span class="chess_websocket">(Websocket)</span></li>
-    </ul>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="js/main.js"></script>
+    <table cellspacing="0" cellpadding="0" class="table">
+      <tr>
+        <td class="canvas_cell">
+        </td>
+        <td class="menu_cell" align="top">
+          <table class="table2">
+            <tr>
+              <td class="chat_cell" align="center" colspan="2">
+              </td>
+              <td>
+              </td>
+            </tr>
+            <tr>
+              <td class="input_cell" align="center" colspan="2">
+              </td>
+              <td>
+              </td>
+            </tr>
+            <tr>
+              <td class="connect_cell" align="center">
+              </td>
+              <td class="disconnect_cell" align="center">
+              </td>
+            </tr>
+            <tr>
+              <td class="sp_cell" colspan="2" align="center">
+              </td>
+              <td>
+              </td>
+            </tr>
+            <tr>
+              <td class="white_cell" align="center">
+              </td>
+              <td class="black_cell" align="center">
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
   </body>
 </html>
