31919d7e06bbafc6035ff708af6b7c3dcb57fcb0
oliegjio <oliegjio@gmail.com>
2016-05-11 22:10:43 +0300

    a

--- proj/shipwars/src/Main.ts
+++ proj/shipwars/src/Main.ts
@@ -8,32 +8,16 @@ import * as Var from './Var';
 class Main {
     constructor(){
         requestAnimationFrame(<any>this.update);
-        this.onresize();
     }
-    private HTMLController: HTMLControl = new HTMLControl();
+    public HTMLController: HTMLControl = new HTMLControl();
+    private numberOfTurns: number = 0;
     private turn: boolean;
     private board: Board = new Board()
     private player1: Player = new Player()
     private player2: Player = new Player()
-    private onresize: ()=> void = function(){
-        window.onresize = function(event){
-            Var.forUpdate.canvasHeight = window.innerHeight;
-            Var.forUpdate.canvasWidth = window.innerHeight;
-        }
-    }
     private update: ()=> void = function(){
-        console.clear();
-        console.log('lol');
+        
     }
 }
 
 let game: Main = new Main();
-
-let canvas: any = $('#canvas')
-    .css({
-        'border': Var.canvasBorderWidth + 'px ' + Var.canvasBorderStyle + ' ' + Var.canvasBorderColor,
-        'height': window.innerHeight - Var.canvasBorderWidth * 3 + 'px',
-        'width': Var.forUpdate.canvasWidth
-    });
-let context: any = canvas.get(0).getContext('2d');
-
--- proj/shipwars/src/Player.ts
+++ proj/shipwars/src/Player.ts
@@ -1,3 +1,5 @@
+import * as Var from './Var';
+
 export class Player {
-    
+    public name: Var.Owner;
 }
